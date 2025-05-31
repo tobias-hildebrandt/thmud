4735ec94e4359d05e9b493295e9c66423beabc9f
Tobias Hildebrandt <tobiash@tobiash.dev>
2025-05-31 17:39:08 +0200

    add collisions and walls via rapier

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,11 @@
 use bevy::{
     DefaultPlugins,
     app::{App, FixedUpdate, Startup},
+    math::Vec2,
+};
+use bevy_rapier2d::{
+    plugin::{NoUserData, RapierConfiguration, RapierContextInitialization, RapierPhysicsPlugin},
+    render::RapierDebugRenderPlugin,
 };
 use thmud::{input_quit, movement, startup_spawn};
 
@@ -8,6 +13,17 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, startup_spawn)
+        .add_plugins({
+            let mut config = RapierConfiguration::new(1.);
+            config.gravity = Vec2::ZERO;
+            RapierPhysicsPlugin::<NoUserData>::default().with_custom_initialization(
+                RapierContextInitialization::InitializeDefaultRapierContext {
+                    integration_parameters: Default::default(),
+                    rapier_configuration: config,
+                },
+            )
+        })
+        .add_plugins(RapierDebugRenderPlugin::default())
         .add_systems(FixedUpdate, (movement, input_quit))
         .run();
 }
