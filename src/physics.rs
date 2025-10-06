7c8178c823f8629d1473583e4c1fb2db50de399b
Tobias Hildebrandt <tobiash@tobiash.dev>
2025-10-06 12:28:04 +0000

    fix max movespeed (#11)
    add boost
    make sure physics objects come to rest at low velocities

--- src/physics.rs
+++ src/physics.rs
@@ -1,9 +1,17 @@
-use bevy::ecs::system::Query;
+use bevy::{ecs::system::Query, math::Vec2};
 use bevy_rapier2d::prelude::Velocity;
+
+const FRICTION_COEFFICIENT: f32 = 0.9;
+const STOP_SPEED: f32 = 0.5;
 
 pub fn friction(mut query: Query<&mut Velocity>) {
     for mut vel in &mut query {
-        vel.linvel *= 0.9;
-        vel.angvel *= 0.9;
+        vel.linvel *= FRICTION_COEFFICIENT;
+        vel.angvel *= FRICTION_COEFFICIENT;
+
+        // make sure objects come to a stop
+        if vel.linvel.length() < STOP_SPEED {
+            vel.linvel = Vec2::ZERO;
+        }
     }
 }
