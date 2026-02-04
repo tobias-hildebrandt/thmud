134c804cf3d5f9244dd0b209da3c3b194e617fc4
Tobias Hildebrandt <tobiash@tobiash.dev>
2026-02-04 19:49:24 +0100

    move rendering to widget

--- crates/sync-test/src/frontend/tui.rs
+++ crates/sync-test/src/frontend/tui.rs
@@ -1,12 +1,12 @@
 use std::time::Duration;
 
 use ratatui::{
-    DefaultTerminal, Frame,
+    DefaultTerminal,
     crossterm::event::{self, KeyCode, KeyEvent, KeyEventKind, KeyModifiers},
     layout::{Constraint, Direction, Layout},
     style::Stylize,
     text::{Line, Text},
-    widgets::{Block, Paragraph},
+    widgets::{Block, Paragraph, Widget},
 };
 
 use crate::simulation::{config::SyncTestConfig, sim::Sim};
@@ -31,7 +31,8 @@ impl Tui {
 
     pub fn run(&mut self, term: &mut DefaultTerminal) -> std::io::Result<()> {
         while !self.exit {
-            term.draw(|frame| self.draw(frame))?;
+            term.draw(|frame| frame.render_widget(&*self, frame.area()))?;
+
             if self.auto_tick {
                 if event::poll(self.auto_tick_duration)? {
                     self.handle_events()?;
@@ -46,51 +47,6 @@ impl Tui {
         }
 
         Ok(())
-    }
-
-    fn draw(&self, frame: &mut Frame) {
-        let title = Line::from(" sync-test TUI ").bold();
-
-        const AUTO_TICK_TEXT: &str = " Auto-tick ";
-        let keybinds = Line::from(vec![
-            " Quit ".into(),
-            "<Ctrl+C/Ctrl+Q>".blue().bold(),
-            if self.auto_tick {
-                AUTO_TICK_TEXT.green()
-            } else {
-                AUTO_TICK_TEXT.red()
-            },
-            "<T>".blue().bold(),
-            " Manual tick ".into(),
-            "<Space>".blue().bold(),
-        ]);
-
-        let top_text = Text::from(vec![title, keybinds]).centered();
-        let paragraph = Paragraph::new(top_text).centered();
-
-        let server_world =
-            Paragraph::new(Text::from(format!("{}", self.sim.server_world)).centered())
-                .centered()
-                .block(Block::bordered().title(Line::from(" server ").centered()));
-        let client_world =
-            Paragraph::new(Text::from(format!("{}", self.sim.client_world)).centered())
-                .centered()
-                .block(Block::bordered().title(Line::from(" client ").centered()));
-
-        let top_level_layout = Layout::default()
-            .direction(Direction::Vertical)
-            .constraints(vec![Constraint::Length(2), Constraint::Fill(2)]);
-
-        let screens_layout = Layout::default()
-            .direction(Direction::Horizontal)
-            .constraints(vec![Constraint::Percentage(50), Constraint::Percentage(50)]);
-
-        let [top_bar, main_screen] = top_level_layout.areas(frame.area());
-        let [server_screen, client_screen] = screens_layout.areas(main_screen);
-
-        frame.render_widget(paragraph, top_bar);
-        frame.render_widget(client_world, client_screen);
-        frame.render_widget(server_world, server_screen);
     }
 
     fn handle_events(&mut self) -> std::io::Result<()> {
@@ -120,5 +76,54 @@ impl Tui {
 
             _ => {}
         }
+    }
+}
+
+impl Widget for &Tui {
+    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
+    where
+        Self: Sized,
+    {
+        let title = Line::from(" sync-test TUI ").bold();
+
+        const AUTO_TICK_TEXT: &str = " Auto-tick ";
+        let keybinds = Line::from(vec![
+            " Quit ".into(),
+            "<Ctrl+C/Ctrl+Q>".blue().bold(),
+            if self.auto_tick {
+                AUTO_TICK_TEXT.green()
+            } else {
+                AUTO_TICK_TEXT.red()
+            },
+            "<T>".blue().bold(),
+            " Manual tick ".into(),
+            "<Space>".blue().bold(),
+        ]);
+
+        let top_paragraph = Paragraph::new(Text::from(vec![title, keybinds]).centered()).centered();
+
+        let server_world =
+            Paragraph::new(Text::from(format!("{}", self.sim.server_world)).centered())
+                .centered()
+                .block(Block::bordered().title(Line::from(" server ").centered()));
+        let client_world =
+            Paragraph::new(Text::from(format!("{}", self.sim.client_world)).centered())
+                .centered()
+                .block(Block::bordered().title(Line::from(" client ").centered()));
+
+        let top_level_layout = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(vec![Constraint::Length(2), Constraint::Fill(2)]);
+
+        let screens_layout = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(vec![Constraint::Percentage(50), Constraint::Percentage(50)]);
+
+        let [top_bar, main_screen] = top_level_layout.areas(area);
+        let [server_screen, client_screen] = screens_layout.areas(main_screen);
+
+        top_paragraph.render(top_bar, buf);
+        client_world.render(client_screen, buf);
+        server_world.render(server_screen, buf);
     }
 }
